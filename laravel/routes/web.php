b978911fe3bec8d1a6501ae7b15768c3621b28ed
bykeat <bykeat@users.noreply.github.com>
2023-06-11 13:11:49 +0000

    Vite & Breeze

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\EventController;
-use App\Models\Event;
-use App\Services\Paginator;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,52 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::prefix('/events')->group(function () {
-
-    //List Events
-    Route::get('/page/{page}', function ($currentPage) {
-        $paginator = App::make(Paginator::class);
-
-        $activeEvents = Event::where('deleted_at', null);
-        $totalRows = $activeEvents->count();
-
-        list($prevPage, $nextPage, $skip, $pageLimit) = $paginator->getPagingNumber($currentPage, $totalRows);
-        if (Cache::getDefaultDriver() == 'redis' && Cache::tags("events")->has("event-list-$currentPage")) {
-            $events = Cache::tags("events")->get("event-list-$currentPage");
-        } else {
-            $events = $activeEvents->limit($pageLimit)
-                ->skip($skip)
-                ->get();
-            if (Cache::getDefaultDriver() == 'redis') {
-                Cache::tags('events')->put("event-list-$currentPage", $events, '30');
-            }
-        }
-
-        return view('events.list', [
-            'events' => $events,
-            'count' => $totalRows,
-            'prevPage' => $prevPage ?: 0,
-            'nextPage' => $nextPage ?: 0,
-        ]);
-    });
-
-    //Create Event
-    Route::get('/create', function () {
-        return view('events.create');
-    });
-
-    //View Event
-    Route::get('/{id}', function ($id) {
-        return view('events.view', [
-            'event' => Event::find($id)
-        ]);
-    });
-});
-
-Route::resource('events', EventController::class)
-    ->only(['index', 'store', 'edit', 'update', 'destroy']); //->middleware(['auth', 'verified']);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -79,30 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/pull-api-data', function () {
-    $client = new GuzzleHttp\Client(['verify' => false]);
-
-    $response = $client->request('get', 'https://api.publicapis.org/entries');
-
-    //Generate column header
-    $contents =  $response->getBody()->getContents();
-    $contentType = $response->getHeader('Content-Type')[0];
-    if (!empty($contents) && strpos($contentType, 'json') !== false) {
-        $contents = json_decode($contents);
-        $contentEntries = $contents->entries;
-        $headers = [];
-        foreach (reset($contentEntries) as $header => $value) {
-            $headers[] = $header;
-        }
-    } else {
-        $contents = [];
-    }
-
-    return view('misc.api_data', [
-        'content-type' => $contentType,
-        'headers' => $headers ?: [],
-        'body' => $contentEntries,
-    ]);
-});
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
